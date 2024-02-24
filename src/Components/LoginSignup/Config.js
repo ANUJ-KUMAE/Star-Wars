8109f807ea56e633811f0c7837fdc6b3923b27ad
Anuj Kumar <119961686+ANUJ-KUMAE@users.noreply.github.com>
2024-02-25 05:04:35 +0530

    First Commit

--- src/Components/LoginSignup/Config.js
+++ src/Components/LoginSignup/Config.js
@@ -2,14 +2,14 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth"
 
-// Your web app's Firebase configuration
+// Add Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA3vVFmWkZ2266YeM7fejK3HU1ucGS2ulY",
-  authDomain: "planetdata-66ca1.firebaseapp.com",
+  apiKey: "",
+  authDomain: "",
   projectId: "planetdata-66ca1",
   storageBucket: "planetdata-66ca1.appspot.com",
   messagingSenderId: "1076275773419",
-  appId: "1:1076275773419:web:1823919662a88dc6fca3ae"
+  appId: ""
 };
 
 // Initialize Firebase
